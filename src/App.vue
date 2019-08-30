22169d1a245a02ae67e3493d6aa9813d1194bdd2
Steve CARRE MDC <steve.carre@gendarmerie.interieur.gouv.fr>
2019-08-30 14:56:55 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UserTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserTable from './components/UserTable'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    UserTable
   }
 }
 </script>
@@ -21,8 +20,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 2%;
 }
 </style>
+
---
+++ src/components/UserTable.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">Tableau d'utilisateurs</h1>
+        <table class="table table-sm table-striped ">
+            <thead class="thead-light">
+                <tr>
+                    <th>Id</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                </tr>
+            </thead>
+            <tr v-for="user in users" 
+                :key="user.id">
+                <td>{{user.id}}</td>
+                <td>{{user.firstname}}</td>
+                <td>{{user.lastname}}</td>
+                <td>{{user.age}}</td>
+                <td>{{user.gender.name}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'UserTable',
+    data() {
+        return {
+            users: []
+
+        }
+    },
+    beforeCreate(){
+        axios.get("http://localhost:9090/Users").then(response => {
+            if (response.status === 200){
+                this.msg = "J'ai une réponse"
+                this.users = response.data
+            } else {
+                this.msg = "J'ai pas de réponse"
+            }
+        }).catch(error => {
+            this.msg = error
+        })
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
